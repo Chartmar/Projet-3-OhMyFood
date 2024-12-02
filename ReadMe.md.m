0c7ca16f472b9525cb7f6eb4fc285bbfe55c4090
Charlotte <charlotte.commissaire@hotmail.com>
2024-12-02 11:56:12 +0100

    Read Me complete

--- ReadMe.md.m
+++ ReadMe.md.m
@@ -1,1 +1,15 @@
 Read Me de mon projet 3 - OhMyFood
+
+HTML :
+Les pages Index + Les 4 pages de menus se trouvent dans le dossier HTML 
+
+CSS : 
+le style est compilé dans le fichier main.CSS
+
+Styles : 
+Les fichiers partiels SCSS sont dans le dossier Styles, classés par sous-dossiers 
+- Base : reset / colors / mixin --> rassemblés dans un partiel _base-all
+- Components : buttons / loader --> rassemblés dans un partiel _components-all
+- Layout: flex / keyframs --> rassemblés dans un partiel _layout-all
+- Pages : home / menus / header-footer --> rassemblés dans un partiel _pages-all
+- Utilities: widht --> rassemblés dans un partiel _utilities-all
